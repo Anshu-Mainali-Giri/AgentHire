b2b35e94ecb52ef5cb011f985a55e647986d5877
Anshu Mainali <anshumainaligiri@gmail.com>
2025-08-29 14:19:39 +1000

    Created New Job Post page and Created Aura Component Wrapper to Override New button of Job Post

--- force-app/main/default/lwc/createJobPost/createJobPost.js
+++ force-app/main/default/lwc/createJobPost/createJobPost.js
@@ -1,3 +1,28 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class CreateJobPost extends LightningElement {}
+export default class CreateJobPost extends NavigationMixin(LightningElement) {
+    handleSuccess(event) {
+        const recordId = event.detail.id;
+
+        // Show Sucess message when new job is created
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: 'Job Post created successfully!',
+                variant: 'success'
+            })
+        );
+
+        // Redirect to the new record page when Post button is clicked
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: recordId,
+                objectApiName: 'Job_Post__c',
+                actionName: 'view'
+            }
+        });
+    }
+}
